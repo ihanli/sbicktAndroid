5cba7bf22ffd3f787c77c3e7ea36556b65f49e76
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2010-12-26 19:21:37 +0100

    created xml file for the main menu of sbickt and added one item

--- gen/geotag/core/R.java
+++ gen/geotag/core/R.java
@@ -15,6 +15,7 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout01=0x7f060000;
+        public static final int newPost=0x7f060002;
         public static final int surface=0x7f060001;
     }
     public static final class layout {
