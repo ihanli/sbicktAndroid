fd034568d7221f28b6b866dc7edfd7ca314bd68e
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2010-12-26 20:27:57 +0100

    added main menu items and corresponding icons

--- gen/geotag/core/R.java
+++ gen/geotag/core/R.java
@@ -11,11 +11,19 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int archiv=0x7f020000;
+        public static final int bicks=0x7f020001;
+        public static final int freunde=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int profil=0x7f020004;
     }
     public static final class id {
         public static final int RelativeLayout01=0x7f060000;
-        public static final int newPost=0x7f060002;
+        public static final int archive=0x7f060005;
+        public static final int friends=0x7f060006;
+        public static final int newPost=0x7f060003;
+        public static final int profile=0x7f060004;
+        public static final int sbicktmenu=0x7f060002;
         public static final int surface=0x7f060001;
     }
     public static final class layout {
