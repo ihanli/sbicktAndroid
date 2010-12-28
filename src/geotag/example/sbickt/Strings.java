ec6053c985c9c8cac66a14d7be9c15266bb7b588
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2010-12-28 12:15:43 +0100

    extracted static strings into Strings class

---
+++ src/geotag/example/sbickt/Strings.java
@@ -0,0 +1,23 @@
+package geotag.example.sbickt;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Strings {
+	private static final String BUNDLE_NAME = "geotag.example.sbickt.strings"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);
+
+	private Strings() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		}
+		catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
