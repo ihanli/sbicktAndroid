a6d855b2a1fefbf331ed900440133818735cd1fe
Ismail Hanli <ihanli.mmt-b2009@fh-salzburg.ac.at>
2010-12-29 16:22:39 +0100

    implemented delete method to HttpHelper class.

--- src/geotag/core/HttpHelper.java
+++ src/geotag/core/HttpHelper.java
@@ -1,19 +1,24 @@
 package geotag.core;
 
+import geotag.example.sbickt.Strings;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
 
 public class HttpHelper {
@@ -21,6 +26,7 @@ public class HttpHelper {
 	private HttpClient client = new DefaultHttpClient();
 	private HttpGet clientGetRequest;
 	private HttpPost clientPostRequest;
+	private HttpDelete clientDeleteRequest;
 	private HttpResponse serverResponse;
 	
 	public HttpHelper(String url){
@@ -80,6 +86,22 @@ public class HttpHelper {
 		try {
 			clientPostRequest.setEntity(new UrlEncodedFormEntity(geoTagData, HTTP.UTF_8));
 			serverResponse = client.execute(clientPostRequest);
+		}
+		catch (Exception e) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public Boolean DELETERequest(Integer geoTagId){
+		try{
+			String deleteURL = Strings.getString("SbicktAPI.2") + geoTagId.toString();
+			List<NameValuePair> geoTagData = new ArrayList<NameValuePair>();
+			geoTagData.add(new BasicNameValuePair("id", geoTagId.toString()));
+			
+			clientDeleteRequest = new HttpDelete(deleteURL);
+			serverResponse = client.execute(clientDeleteRequest);
 		}
 		catch (Exception e) {
 			return false;
